936d2bea48de6bbfa715a1247c4e753af2c5139f
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2015-06-18 17:23:33 -0300

    adicionando classes

---
+++ src/main/java/com/magnumopus/managedbeans/CarMB.java
@@ -0,0 +1,16 @@
+package com.magnumopus.managedbeans;
+
+import com.magnumopus.models.Car;
+
+public class CarMB {
+	
+	private Car car = new Car();
+
+	public Car getCar() {
+		return car;
+	}
+
+	public void setCar(Car car) {
+		this.car = car;
+	}
+}
---
+++ src/main/java/com/magnumopus/models/Car.java
@@ -0,0 +1,20 @@
+package com.magnumopus.models;
+
+public class Car {
+	
+	private String model;
+	private String brand;
+	
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+}
